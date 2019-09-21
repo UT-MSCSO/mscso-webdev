cdc51f67cbe6eeca15a1af0568cabba4686eddb5
Rita Zhao <Rita.Zhao@ibm.com>
2019-09-21 09:44:10 -0500

    add some views

---
+++ src/views/DashboardView.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+
+const DashboardView = () => {
+  useEffect(() => {
+  }, []);
+
+  return (
+    <div>
+      this is a stupid div
+    </div>
+  );
+};
+
+DashboardView.defaultProps = {
+};
+
+DashboardView.propTypes = {
+};
+
+export default DashboardView;
